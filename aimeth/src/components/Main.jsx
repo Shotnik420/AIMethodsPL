87665464c75f88963e8e3a9a6d91147acb4057af
Kubatoja <jakub1720@gmail.com>
2024-06-02 16:01:45 +0200

    ww

--- aimeth/src/components/Main.jsx
+++ aimeth/src/components/Main.jsx
@@ -1,24 +1,62 @@
 import "../styles/Main.css";
-import videoPath from "../img/robot.mp4";
-import { BsChevronCompactDown } from "react-icons/bs";
-import logo from "../img/logo.png";
+import { FiArrowUpRight } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
+
 // To jest kod do głównej karty strony głównej.
 function Main() {
+  let navigate = useNavigate();
+  const routeChange = () => {
+    let path = `/silesianphoenix`;
+    navigate(path);
+  };
   return (
     <div className="Main">
-      <div className="mainLogo">
-        <video src={videoPath} autoPlay loop muted />
-        <div className="mainLogoContent">
-          <img src={logo} />
-          <h1>
-            {" "}
-            <strong>AI-Meth</strong>{" "}
-          </h1>
-          <div className="mainGoDown">
-            <div className="kreska"></div>
-            <a href="#projekty">
-              <BsChevronCompactDown className="goDown" />
-            </a>
+      <div className="mainBackground">
+        <div className="silesianPhoenix">
+          <div className="SPsection">
+            <div className="SPlogo"></div>
+            <div className="SPdesc">
+              <b className="orange">Silesian Phoenix </b> to drużyna, którą
+              tworzą studenci należący do międzywydziałowego SKN Zastosowania
+              Metod Sztucznej Inteligencji AI - METH. {" "}
+            </div>
+            <div
+              className="mainBtn"
+              onClick={routeChange}
+              style={{ backgroundColor: "#FF7A00" }}
+            >
+              Przeczytaj więcej <FiArrowUpRight className="arrow" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="events">
+        <Event
+          date="11 Listopad"
+          type="Konkurs"
+          name="Canadian rover challange"
+        />
+        <Event date="11 Grudzień" type="Wydarzenie" name="GOES-U" />
+        <Event date="24 Grudzień" type="Święta" name="Boże narodzenie" />
+      </div>
+    </div>
+  );
+}
+
+function Event({ date, type, name }) {
+  return (
+    <div className="event">
+      <div className="eventDate">{date}</div>
+      <div className="eventLine"></div>
+      <div className="EventContainer">
+        <div className="left">
+          {" "}
+          <div className="eventType">{type}</div>
+          <div className="eventName">{name}</div>
+        </div>
+        <div className="right">
+          <div className="eventBtn">
+            <FiArrowUpRight />
           </div>
         </div>
       </div>
