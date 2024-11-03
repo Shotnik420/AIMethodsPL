42c502418c068a39ffb5af511fcf4efb52610631
SZOOTNIK* <62155481+Shotnik420@users.noreply.github.com>
2024-11-03 21:58:13 +0100

    DockerFile for backend added, changed bcrypt to bcryptjs

--- back/passport-config.js
+++ back/passport-config.js
@@ -1,5 +1,5 @@
 const LocalStrategy = require("passport-local").Strategy;
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 function initialize(passport, getUserByUsername, getUserById) {
   const authenticateUser = async (username, password, done) => {
